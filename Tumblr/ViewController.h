04e65bf55319bc321cae42b1fc1fd96fd267057f
Jennifer Beck <jjbeck@yahoo-inc.com>
2017-01-25 10:17:03 -0800

    Initial Commit

---
+++ Tumblr/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tumblr
+//
+//  Created by Jennifer Beck on 1/25/17.
+//  Copyright © 2017 JenniferBeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Tumblr/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Tumblr
+//
+//  Created by Jennifer Beck on 1/25/17.
+//  Copyright © 2017 JenniferBeck. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
